e7909131dde058b4985d592188f1b6179b38d18c
DESKTOP-KAT0B72\mapzi <mapzine123@naver.com>
2022-03-29 01:54:07 +0900

    weather API

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,24 @@
+
+const API_KEY = "88d4321e2be985d32566d965d55e1700";
+
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log(lat, lng);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data.name);
+        console.log(data.weather[0].main);
+        console.log(data.main.temp);
+        
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        console.dir(city);
+        city.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
